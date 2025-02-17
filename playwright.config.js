9af515ae5011942f401589295160a6016acc080f
Rasikas221 <rasikashanmugham@gmail.com>
2025-02-17 15:00:38 +0530

    library file open push

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,7 +31,7 @@ export default defineConfig({
   timeout: 2 * 100 * 2000,
  
   expect: {
-    timeout: 5 * 2000,
+    timeout: 30 * 2000,
   },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
